e219d738440d08360c3ee0ba182c85b5e89a55da
Baba Shree Baniya <babashreebaniya@gmail.com>
2025-08-02 13:51:11 +0545

    changes

--- latest/src/contexts/CartContext.jsx
+++ latest/src/contexts/CartContext.jsx
@@ -34,8 +34,6 @@ export const CartProvider = ({ children }) => {
   const clearCart = () => setCart([]);
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
-      {children}
-    </CartContext.Provider>
+   
   );
 }; 
